6a40941768e86fcbccfe33e676c2a1d2bc47015f
kousthubh <kyadavalli04@gmail.com>
2025-08-11 18:16:50 +0530

    pushing model and api

---
+++ api/api.py
@@ -0,0 +1,45 @@
+from fastapi import FastAPI ,File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+from io import BytesIO
+from PIL import Image
+import numpy as np
+import tensorflow as tf
+
+app = FastAPI()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],  # React dev server
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+MODEL= tf.keras.models.load_model("../models/1.keras")
+CLASS_NAMES=['Healthy','Late Blight','Early Blight']
+@app.get("/")
+async def read_root():
+    return "Hello world"
+
+def read_file_as_image(data)->np.ndarray:
+    image=np.array(Image.open(BytesIO(data)))
+    return image
+
+@app.post("/predict")
+async def predict(file:UploadFile=File(...)):
+    image=read_file_as_image(await file.read())
+    img_batch=np.expand_dims(image, axis=0)
+    predictions=MODEL.predict(img_batch)
+    
+    predicted_class=CLASS_NAMES[np.argmax(predictions[0])]
+    confidence_score=np.max(predictions[0])
+    return{
+        "class": predicted_class,
+        "confidence": float(confidence_score)
+    }
+    
+    
+    
+    
